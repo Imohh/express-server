d36a32dc591534373e926c7a8b706a72bcf079ef
Imoh Precious <jaypee88830@gmail.com>
2023-06-29 17:40:57 +0100

    user model

--- routes/auth.js
+++ routes/auth.js
@@ -1,37 +1,57 @@
 const router = require("express").Router()
 const { User } = require("../models/user")
-const Joi = require("joi")
-const bcrypt = require("bcrypt")
+const CryptoJS = require("crypto-js");
+
+
+
 
 router.post("/", async(req, res) => {
+// 	try {
+// 		const { error } = validate(req.body)
+// 		if(error)
+// 			return res.status(400).send({message: error.details[0].message})
+// 		const user = await User.findOne({ email: req.body.email })
+// 		if(!user)
+// 			return res.status(401).send({message: "Invalid Email or Password"})
+
+// 		const validPassword = await bcrypt.compare(
+// 			req.body.password, user.password
+// 		)
+// 		if(!validPassword)
+// 			return res.status(401).send({message: "Invalid Email or Password"})
+
+// 		const token = user.generateAuthToken()
+// 		res.status(200).send({data: token, message: "Logged in succesfully"})
+
+// 	} catch (error) {
+// 		res.status(500).send({message: "Internal Server Error"})
+// 	}
+// })
+
+// const validate = (data) => {
+// 	const schema = Joi.object ({
+// 		email: Joi.string().email().required().label("Email"),
+// 		password: Joi.string().required().label("Password")
+// 	})
+// 	return schema.validate(data)
+// }
+
+
 	try {
-		const { error } = validate(req.body)
-		if(error)
-			return res.status(400).send({message: error.details[0].message})
-		const user = await User.findOne({ email: req.body.email })
-		if(!user)
-			return res.status(401).send({message: "Invalid Email or Password"})
+		const user = await User.findOne({ fullName: req.body.fullName })
+		!user && res.status(401).json("wrong credentials")
 
-		const validPassword = await bcrypt.compare(
-			req.body.password, user.password
-		)
-		if(!validPassword)
-			return res.status(401).send({message: "Invalid Email or Password"})
+		const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC)
+		const password = hashedPaasword.toString(CryptoJS.enc.Utf8)
 
-		const token = user.generateAuthToken()
-		res.status(200).send({data: token, message: "Logged in succesfully"})
+		password != req.body.password && 
+			res.status(401).json("Wrong credentials")
 
-	} catch (error) {
-		res.status(500).send({message: "Internal Server Error"})
+		res.status(200).json(user)
+	} catch (err) {
+		res.status(500).json(err)
 	}
-})
 
-const validate = (data) => {
-	const schema = Joi.object ({
-		email: Joi.string().email().required().label("Email"),
-		password: Joi.string().required().label("Password")
-	})
-	return schema.validate(data)
-}
+})
 
 module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const router = require("express").Router()
 const {User, validate} = require("../models/user")
-const bcrypt = require("bcrypt")
 const CryptoJS = require("crypto-js");
 
 router.post("/", async(req, res) => {
