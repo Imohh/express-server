51eb75678f891bb9bdb2c6a2aa14cdf53a683b4e
Imoh Precious <jaypee88830@gmail.com>
2023-06-29 03:55:16 +0100

    corrections

--- routes/users.js
+++ routes/users.js
@@ -5,12 +5,14 @@ const bcrypt = require("bcrypt")
 router.post("/", async(req, res) => {
 	try {
 		const{ error } = validate(req.body)
-		if(error)
+		if(error) {
 			return res.status(400).send({ message: error.details[0].message })
+		}
 
 		const user = await User.findOne({ email: req.body.email })
-		if(user)
+		if(user) {
 			return res.status(409).send({message: "User with given email already exists"})
+		}
 
 		const salt = await bcrypt.genSalt(Number(process.env.SALT))
 		const hashPassword = await bcrypt.hash(req.body.password, salt)
